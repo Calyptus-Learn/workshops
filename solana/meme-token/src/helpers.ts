e657bf179a8bc583a9cb724f189242c492efcc31
Jimii <jimmyimpulse2@gmail.com>
2024-04-10 16:29:01 +0300

    update

--- solana/meme-token/src/helpers.ts
+++ solana/meme-token/src/helpers.ts
@@ -3,7 +3,7 @@ import { Keypair } from "@solana/web3.js";
 import { readFileSync } from "fs";
 import { homedir } from "os";
 
-const USER_KEYPAIR_PATH = homedir() + "/.config/solana/id-new.json";
+const USER_KEYPAIR_PATH = homedir() + "/.config/solana/id.json";
 export const userKeypair = Keypair.fromSecretKey(
 	Buffer.from(JSON.parse(readFileSync(USER_KEYPAIR_PATH, "utf-8")))
 );
